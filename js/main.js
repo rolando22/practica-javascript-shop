bb3f340da56062fe6ccf3bbad7bf03b595c74175
rolando22 <rolando2207@gmail.com>
2023-01-29 22:06:36 -0300

    [FEAT] funcionalidad de toggle para el carrito de compras

--- js/main.js
+++ js/main.js
@@ -2,15 +2,25 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const btnMobileMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const btnNavCart = document.querySelector('.navbar-shopping-cart');
+const cart = document.querySelector('.shopping-cart-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 btnMobileMenu.addEventListener('click', toggleMobileMenu);
+btnNavCart.addEventListener('click', toggleCart);
 
 function toggleDesktopMenu () {
+    cart.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
-    console.log('Llgué');
+    cart.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCart () {
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    cart.classList.toggle('inactive');
 }
