d02e30266e741d172b11ffa2959a1f1926d3415c
rolando22 <rolando2207@gmail.com>
2023-01-29 21:21:10 -0300

    [FEAT] funcionalidad de toggle para el menu mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const btnMobileMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+btnMobileMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu () {
+    console.log('Llgué');
+    mobileMenu.classList.toggle('inactive');
 }
